1bb52576e094ae2c6f73b0f39695e1594063341e
Edison-5 <edisoncando060@gmail.com>
2023-05-28 12:38:45 -0500

    Esta es una calculadora de prueba

---
+++ relo.py
@@ -0,0 +1,8 @@
+import datetime
+import time
+
+while True:
+    now = datetime.datetime.now()
+    print(now.strftime("%H:%M:%S"), end="", flush=True)
+    print("\r", end="", flush=True)
+    time.sleep(1)
